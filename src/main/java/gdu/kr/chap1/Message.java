9c766f73411eba8e5dd5d9642dc707b06438a638
GDJ 62 <GDJ 62@DESKTOP-NA0T4J2>
2023-05-16 17:16:47 +0900

    chap1

---
+++ src/main/java/gdu/kr/chap1/Message.java
@@ -0,0 +1,6 @@
+package gdu.kr.chap1;
+
+public interface Message {
+	void sayHello(String msg);
+
+}
---
+++ src/main/java/gdu/kr/chap1/MessageEn.java
@@ -0,0 +1,8 @@
+package gdu.kr.chap1;
+
+public class MessageEn implements Message{
+	public void sayHello(String msg) {
+		System.out.println("Hello" + msg );
+	}
+
+}
